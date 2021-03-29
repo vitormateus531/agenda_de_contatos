2f98f7f8226e52d79e45077b4581c524eb8f414f
Vitor Mateus <vitormateus15@hotmail.com>
2021-03-29 00:50:28 -0300

    ajustes forms

--- resources/views/livewire/edit-one-contact.blade.php
+++ resources/views/livewire/edit-one-contact.blade.php
@@ -12,11 +12,11 @@
                     <div class="divide-y divide-gray-200">
                         <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                             <div class="flex flex-col">
-                                <label class="leading-loose">Nome</label>
+                                <label class="leading-loose">Nome*</label>
                                 <input type="text" name="nome" wire:model="nome" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Nome">
                             </div>
                             <div class="flex flex-col">
-                                <label class="leading-loose">Email</label>
+                                <label class="leading-loose">Email*</label>
                                 <input type="text" name="email" wire:model="email" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Email">
                             </div>
                             <div class="flex items-center space-x-4">
@@ -32,7 +32,7 @@
                                     </div>
                                 </div>
                                 <div class="flex flex-col">
-                                    <label class="leading-loose">Celular</label>
+                                    <label class="leading-loose">Celular*</label>
                                     <div class="relative focus-within:text-gray-600 text-gray-400">
                                         <input type="text" name="celular" wire:model="celular" class="pr-4 pl-10 py-2 border fone focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Celular">
                                         <div class="absolute left-3 top-2">
