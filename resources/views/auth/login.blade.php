2bd94e7b5526faa219e03b15a16859ed2470ade6
Vitor Mateus <vitormateus15@hotmail.com>
2021-03-29 00:34:21 -0300

    ajustes tela login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,10 +27,13 @@
             </div>
 
             <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
+               <!--<label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Lembre-me') }}</span>
-                </label>
+                </label> -->
+                <label for="remember_me" class="flex items-center">
+                    Não possui conta? <a href="{{route('register')}}"><span class="ml-2 text-sm text-gray-600">{{ __('Cadastre-se aqui!') }}</span></a>
+                </label> 
             </div>
 
             <div class="flex items-center justify-end mt-4">
